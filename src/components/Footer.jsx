2bc23802ee8f6983f7366c1e9e321a2d8946587b
UltimateDev <iyiadeboluwatife8@gmail.com>
2025-10-28 01:23:36 +0100

    Little changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/footer.css";
-import Logo from "../assets/images/Dabyles_logo.png"
+import Logo from "../assets/images/Dabylesint.png"
 
 export default function Footer() {
   return (
--- src/components/Theteam.jsx
+++ src/components/Theteam.jsx
@@ -3,7 +3,7 @@ import "../styles/general.css"
 import "../styles/theteam.css"
 import Teamcard from './Teamcard'
 import popoola from "../assets/images/popoola.jpg"
-import noProfile from "../assets/images/noprofile.jpeg"
+import adebayo from "../assets/images/adebayo.jpeg"
 import adesusi from "../assets/images/adesusi.jpg"
 
 const Theteam = () => {
@@ -14,7 +14,7 @@ const Theteam = () => {
       </div>
       
       <div className="team-container">
-        <Teamcard memberName = "Mr. Adedayo Osore" designation = "Director" mail = "dayoosore@dabyles.com " img = {noProfile}/>
+        <Teamcard memberName = "Mr. Adedayo Osore" designation = "Director" mail = "dayoosore@dabyles.com " img = {adebayo}/>
         <Teamcard memberName = "Mr. Abayomi Popoola" designation = "Director" mail = "yomipopoola@dabyles.com" img = {popoola}/>
            <Teamcard memberName = "Dr. Olanrewaju Adesusi" designation = "Director" mail = "lanreadesusi@dabyles.com" img = {adesusi}/>
       </div>
